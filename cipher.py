3c8a4c2020d1192c16ef45159635754a70dc2b78
mandhyan <yash@mandhyan.in>
2017-01-21 02:08:40 +0530

    Only have to take system arguments now

--- cipher.py
+++ cipher.py
@@ -4,13 +4,18 @@ def cipher(message,shift):
 	for ch in message:
 		if ch == ' ':
 			new_msg=new_msg+' '
-		else:
-			if ch >= 'A' and ch <= 'Z':
-				num=ord(ch)+shift
-				num=num-65
-				num=num%26
-				num=num+65
-				new_msg=new_msg+chr(num)
+		elif ch >= 'A' and ch <= 'Z':
+			num=ord(ch)+shift
+			num=num-65
+			num=num%26
+			num=num+65
+			new_msg=new_msg+chr(num)
+		elif ch >= 'a' and ch <='z':
+			num=ord(ch)-shift
+			num=num-97
+			num=num%26
+			num=num+95
+			mew_msg=new_msg+chr(num)
 	print new_msg
 def decipher(message,shift):
 	new_msg=''
@@ -18,11 +23,16 @@ def decipher(message,shift):
 	for ch in message:
 		if ch == ' ':
 			new_msg=new_msg+' '
-		else:
-			if ch >= 'A' and ch <='Z':
-				num=ord(ch)-shift
-				num=num-65
-				num=num%26
-				num=num+65
-				new_msg+new_msg+chr(num)
+		elif ch >= 'A' and ch <='Z':
+			num=ord(ch)-shift
+			num=num-65
+			num=num%26
+			num=num+65
+			new_msg+new_msg+chr(num)
+		elif ch >= 'a' and ch <='z':
+			num=ord(ch)-shift
+			num=num-97
+			num=num%26
+			num=num+95
+			mew_msg=new_msg+chr(num)
 	print new_msg
