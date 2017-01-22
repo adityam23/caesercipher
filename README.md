30f3939d5c95b93bae59dccb37f06c3e94d36c2b
mandhyan <yash@mandhyan.in>
2017-01-22 14:15:59 +0530

    Fixed and working form, read README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # caesercipher
 Caeser Cipher implementation
+
+
+Usage->
+For encrypting:
+python cipher.py filename shift cipher
+
+For decrypting:
+python cipher.py filename shift decipher
--- cipher.py
+++ cipher.py
@@ -1,38 +1,53 @@
+import sys
+file=sys.argv[1]
+shift=int(sys.argv[2])
+encode=sys.argv[3]
+f=open(file,'r')
+mess=f.readlines()
+
 def cipher(message,shift):
-	new_msg=""
-	num=0
-	for ch in message:
-		if ch == ' ':
-			new_msg=new_msg+' '
-		elif ch >= 'A' and ch <= 'Z':
-			num=ord(ch)+shift
-			num=num-65
-			num=num%26
-			num=num+65
-			new_msg=new_msg+chr(num)
-		elif ch >= 'a' and ch <='z':
-			num=ord(ch)-shift
-			num=num-97
-			num=num%26
-			num=num+95
-			mew_msg=new_msg+chr(num)
-	print new_msg
+	sm=open('output.txt','w+')
+	for lines in message:
+		new_msg=""
+		num=0
+		for ch in lines:
+			if ch == ' ':
+				new_msg=new_msg+" "
+			elif ch >= 'A' and ch <= 'Z':
+				num=ord(ch)+shift
+				num=num-65
+				num=num%26
+				num=num+65
+				new_msg=new_msg+chr(num)
+			elif ch >= 'a' and ch <='z':
+				num=ord(ch)+shift
+				num=num-97
+				num=num%26
+				num=num+95
+				new_msg=new_msg+chr(num)
+		sm.write(new_msg+'\n')
 def decipher(message,shift):
-	new_msg=''
-	num=0
-	for ch in message:
-		if ch == ' ':
-			new_msg=new_msg+' '
-		elif ch >= 'A' and ch <='Z':
-			num=ord(ch)-shift
-			num=num-65
-			num=num%26
-			num=num+65
-			new_msg+new_msg+chr(num)
-		elif ch >= 'a' and ch <='z':
-			num=ord(ch)-shift
-			num=num-97
-			num=num%26
-			num=num+95
-			mew_msg=new_msg+chr(num)
-	print new_msg
+	dc=open('output.txt','w+')
+	for lines in message:
+		new_msg=""
+		num=0
+		for ch in lines:
+			if ch == ' ':
+				new_msg=new_msg+" "
+			elif ch >= 'A' and ch <='Z':
+				num=ord(ch)-shift
+				num=num-65
+				num=num%26
+				num=num+65
+				new_msg=new_msg+chr(num)
+			elif ch >= 'a' and ch <='z':
+				num=ord(ch)-shift
+				num=num-97
+				num=num%26
+				num=num+97
+				new_msg=new_msg+chr(num)
+		dc.write(new_msg)
+if encode == 'cipher':
+	cipher(mess,shift)
+if encode == 'decipher':
+	decipher(mess,shift)
